c0113edd840e6ce15b807acf6a9719b3afabb73b
Thanapat Veerakajorn <t.veerakajorn@gmail.com>
2020-02-24 01:47:33 +0000

    Automatically backed up by Learn

--- lib/create.sql
+++ lib/create.sql
@@ -9,3 +9,25 @@ INSERT INTO projects (title, category, funding_goal, start_date, end_date) VALUE
 ('Hall of Fame', 'Music', 100000, '6/1/16', '12/1/16'),
 ('I Wrote a book on SQL', 'Book', 5000, '9/1/16', '10/1/16'),
 ("Just kidding, I didn't", 'Book', 6000, '10/1/16', '12/1/16');
+
+INSERT INTO users (name, age) VALUES
+('Finnebar', 17),
+('Bear', 6),
+('Iguana', 4),
+('Alex', 33),
+('Amanda', 24),
+('Sophie', 24),
+('Rosey', 9),
+('Victoria', 23),
+('Franz', 100),
+('Hermione', 30),
+('Voldemort', 90),
+('Marisa', 24),
+('Swizzle', 4),
+('Sirius', 36),
+('Albus', 113),
+('Squid', 5),
+('Whale', 6),
+('Pacha', 5),
+('Ena', 24),
+('Katie', 24);
