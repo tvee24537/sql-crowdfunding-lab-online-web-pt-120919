942d7b5f3d13c7a7a9987c371859e27a488ba40e
Thanapat Veerakajorn <t.veerakajorn@gmail.com>
2020-02-24 01:47:19 +0000

    Automatically backed up by Learn

---
+++ lib/create.sql
@@ -0,0 +1,11 @@
+INSERT INTO projects (title, category, funding_goal, start_date, end_date) VALUES
+('Alpine Lodge', 'Recreation', 100000, '6/1/16', '12/1/16'),
+('Banana Pies', 'Food', 200000, '9/1/16', '12/1/16'),
+('Camel Racing', 'Recreation', 500000, '12/1/16', '12/1/17'),
+('Duck Breeding', 'Book', 50000, '6/15/16', '8/15/16'),
+('Elephant Ears', 'Music', 25000, '7/15/16', '12/15/16'),
+('Franks on a Stick', 'Food', 1000000, '6/1/16', '6/1/17'),
+('Goose in a Box', 'Animals', 5000, '4/1/16', '11/1/16'),
+('Hall of Fame', 'Music', 100000, '6/1/16', '12/1/16'),
+('I Wrote a book on SQL', 'Book', 5000, '9/1/16', '10/1/16'),
+("Just kidding, I didn't", 'Book', 6000, '10/1/16', '12/1/16');
